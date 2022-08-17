c089d60962efd1f0de5013bd379d5f0976d4246d
nadinnah <nadinahmedmohamed@gmail.com>
2022-08-17 14:21:58 +0300

    first work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,27 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.Scanner
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val scn = Scanner(System.`in`)
+    println("Enter the size:")
+    var size = scn.nextInt()
+
+    val myList = mutableListOf<String>()
+    scn.nextLine()
+
+    while (size > 0) {
+        println("enter list inputs:")
+        val inputs = scn.nextLine()
+        myList.add(inputs)
+        size--
+    }
+
+    val filteredValuesM= myList.filter { it.contains("m") }
+    val filteredValuesA= myList.filter { it.contains("a" ) }
+
+   println( filteredValuesM.toString().uppercase())
+   println(filteredValuesA.toString().uppercase())
 }
+
+
+
+
